60186d1b9e1322351c4a3a12482295ce1f03ca18
Stephano <stephanochang21@gmail.com>
2023-11-19 23:07:14 -0500

    Update Program.cs

--- ConautoDT.Web/Program.cs
+++ ConautoDT.Web/Program.cs
@@ -3,13 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
-//builder.Services.AddAuthentication();
-//builder.Services.AddIdentity<IdentityUser, IdentityRole>(opciones => {
-//    opciones.SignIn.RequireConfirmedAccount = false;
-//});
-
 var app = builder.Build();
-
+app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -17,14 +12,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Index}/{id?}");
