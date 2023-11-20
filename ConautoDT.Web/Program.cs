7a1c77ca52f391bb3402a997a56065935eb4fc6f
Stephano <stephanochang21@gmail.com>
2023-11-20 00:04:52 -0500

    Update Program.cs

--- ConautoDT.Web/Program.cs
+++ ConautoDT.Web/Program.cs
@@ -10,9 +10,24 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        OnPrepareResponse = context =>
+        {
+            // Deshabilitar la caché en producción
+            context.Context.Response.Headers["Cache-Control"] = "no-cache, no-store";
+            context.Context.Response.Headers["Pragma"] = "no-cache";
+            context.Context.Response.Headers["Expires"] = "-1";
+        }
+    });
 }
+else
+{
+    app.UseStaticFiles();
+}
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+//app.UseStaticFiles();
 //app.UseRouting();
 app.UseAuthorization();
 app.MapControllerRoute(
