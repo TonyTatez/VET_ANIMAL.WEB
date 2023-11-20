558e7706b50d21155313a83b6e85b15311c7a07d
Stephano <stephanochang21@gmail.com>
2023-11-19 23:30:40 -0500

    Update Program.cs

--- ConautoDT.Web/Program.cs
+++ ConautoDT.Web/Program.cs
@@ -4,16 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
-app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseStaticFiles();
 }
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllerRoute(
