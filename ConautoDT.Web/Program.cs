2834ce7b1048c341c6f760f298ce46e64bfc9b2b
Stephano <stephanochang21@gmail.com>
2023-11-19 23:53:07 -0500

    Update Program.cs

--- ConautoDT.Web/Program.cs
+++ ConautoDT.Web/Program.cs
@@ -13,7 +13,7 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-app.UseRouting();
+//app.UseRouting();
 app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
